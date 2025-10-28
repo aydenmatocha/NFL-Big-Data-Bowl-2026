36ec6cfd653d772da0acd82ff76b6065b3a0123b
Ayden Matocha <aydenmatocha@gmail.com>
2025-10-28 16:44:14 -0400

    changes

--- test.R
+++ test.R
@@ -2,29 +2,48 @@ library(tidyverse)
 library(ggplot2)
 library(gganimate)
 library(sportyR)
+library(nflreadr)
 
-# Get CSV File
+teams_colors_logos <- nflreadr::load_teams()
+head(teams_colors_logos)
+
+# Get CSV Files
 week1 <- read.csv("data/input_2023_w01.csv")
+supplementary <- read.csv("data/supplementary_data.csv")
 
-str(week1)
+merged <- week1 %>%
+  left_join(
+    supplementary %>%
+      select(game_id, play_id, possession_team, defensive_team, home_team_abbr, visitor_team_abbr),
+    by = c("game_id", "play_id")
+  ) %>%
+  mutate(
+    team_abbr = case_when(
+      player_side == "Offense" ~ possession_team,
+      player_side == "Defense" ~ defensive_team,
+      TRUE ~ NA_character_
+    )
+  ) %>%
+  left_join(teams_colors_logos %>% select(team_abbr, team_color, team_color2),
+            by = "team_abbr") %>%
+  mutate(color = team_color)
 
-play <- week1 %>%
-  filter(game_id == 2023090700, play_id == 101) %>%
-  mutate(color = case_when(
-      player_position == "WR" ~ "blue",
-      player_position == "TE" ~ "purple",
-      player_position == "QB" ~ "black",
-      player_position == "CB" ~ "red",
-      player_position == "MLB" ~ "white",
-      player_position == "SS" ~ "yellow",
-      player_position == "FS" ~ "grey"
+
+play <- merged %>%
+  filter(game_id == 2023090700, play_id == 194) %>%
+  mutate(
+    home_or_away = case_when(
+      team_abbr == home_team_abbr ~ "home",
+      team_abbr == visitor_team_abbr ~ "away",
+      TRUE ~ NA_character_
     )
   )
+  
 
 nfl_field <- geom_football("nfl", x_trans = 60, y_trans = 26.6667)
 
 play_anim <- nfl_field +
-  geom_point(data = play, aes(x, y), color = play$color) +
+  geom_point(data = play, aes(x, y, color = color), size = 5) +
   transition_time(frame_id)
 
 animate(play_anim, fps = 10, nframes = max(play$frame_id, na.rm = TRUE))
