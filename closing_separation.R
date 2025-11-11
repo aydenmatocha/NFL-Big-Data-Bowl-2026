f7894e5e25183cef807efc46c2fb78306926b1d1
Ayden Matocha <aydenmatocha@gmail.com>
2025-11-11 14:04:06 -0500

    added methods

--- closing_separation.R
+++ closing_separation.R
@@ -16,6 +16,22 @@ calc_closing_sep <- function(input_df, throw_frame) {
   return(init_sep - final_sep)
 }
 
+get_init_sep <- function(input_df, throw_frame) {
+  init_sep <- input_df %>%
+    filter(frame_id == throw_frame, man_def == TRUE) %>%
+    pull(dist_from_rec)
+  
+  return(init_sep)
+}
+
+get_final_sep <- function(input_df) {
+  final_sep <- input_df %>%
+    filter(frame_id == max(frame_id), man_def == TRUE) %>%
+    pull(dist_from_rec)
+  
+  return(final_sep)
+}
+
 #week1 <- read.csv("data/input_2023_w01.csv") %>%
 #filter(game_id == 2023090700, play_id == 194)
 
